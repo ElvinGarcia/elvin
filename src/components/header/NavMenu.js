3893fea331f42c6c04e3c2ef4799418563cf857c
Elvin <Elvin@ElvinGarcia.com>
2022-10-03 14:22:03 -0400

    restructure nav info and layout

--- src/components/header/NavMenu.js
+++ src/components/header/NavMenu.js
@@ -1,24 +1,44 @@
 import  React,{ Component } from "react";
 import { NavLink } from "react-router-dom";
 
+//    type: 'logo',
+//   src: './assets/images/elvin.svg',
+//   alt: 'site logo',
+//   name: 'Elvin',
+//   url: '/',
+//   svg:svg
 
 class NavMenu extends Component{
 
   render() {
+   const navbarLogo = this.props.images.map((obj, id) => {
+     return obj.type === "logo" ? <NavLink to={obj.url} alt={obj.alt} className="nav logo" key={id}>
+       {obj.svg}
+       <span className="logo">{obj.name}</span>
+     </NavLink> : null
+   });
 
-
+    const site_links = (
+      <ul className="site_links">
+        {
+          this.props.site_links.map((obj, id) => {
+            if (obj.type === "site_links") {
+              return <li key={id} className="list_item"> <NavLink to={obj.url}>{obj.name}</NavLink></li>
+            } else {
+              return null
+            }
+          })
+        }
+      </ul>
+    );
     return (
-      <li className="nav-item">
-        <NavLink
-          to={this.props.url}
-          className="nav-link"
-        >
-          {this.props.name}
-        </NavLink>
-      </li>
-    )
-  }
-}
+      <>
+        {navbarLogo}
+        {site_links}
+      </>
+    );
+  };
+};
 
 
 export default NavMenu
