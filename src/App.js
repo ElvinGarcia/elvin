412114d20c87ef76316dda2f461a1463850b56b6
Elvin <Elvin@ElvinGarcia.com>
2022-10-14 12:18:12 -0400

    added writing route to writing page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from './containers/Layout';
 import Projects from './components/projects/Projects';
 import Main from './components/main/Main';
-// import Writing from './components/writing/Writing';
+import Writing from './components/writing/Writings';
 // import Speaking from './components/speaking/Speaking';
 
 
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Main/>}/>
           <Route path="/Projects" element={<Projects/>}/>
-          {/* <Route path="/Writing" element={Writing }/> */}
+          <Route path="/Writing" element={<Writing/> }/>
           {/* <Route path="/Speaking" element={Speaking}/> */}
         <Route path="*" element={<h1>~~Nothing to see here ~~</h1> }/>
         </Route>
