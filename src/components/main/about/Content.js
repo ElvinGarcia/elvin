53f8c7d02d4b5cdbe1fc81c8af4888cfdceab4eb
Elvin <Elvin@ElvinGarcia.com>
2022-10-24 10:47:47 -0400

    adjusted the container size for the about info section and corrected typo on the class name

--- src/components/main/about/Content.js
+++ src/components/main/about/Content.js
@@ -6,9 +6,9 @@ const Content = (props) => {
     return (
       <>
       <h2> {props.assets.content.title} </h2>
-      <div className='about_me'>
-       <img src={props.assets.images.src} alt={props.assets.images.alt} className="self_portrait" />
+      <div className='about_me info'>
         <p className="content">{props.assets.content.content} </p>
+       <div className="image_container"><img src={props.assets.images.src} alt={props.assets.images.alt} className="self_portrait" /></div>
         </div>
         </>
     )
