cbbf9bbb94508ba51cc51e288e786bcc46d8fe87
Elvin <Elvin@ElvinGarcia.com>
2022-10-25 21:35:28 -0400

    created a welcome screen

--- src/components/splash_screen/Content.js
+++ src/components/splash_screen/Content.js
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 
 
 export default function Content(props) {
+  console.log(props)
 
   return (
     <div className="greeting_screen">
-      <img className="logo" src={props.logo.src} alt={props.logo.alt}></img>
-      <p className="greeting"> {props.greeting} </p>
-       <Link className="call_to_action">{props.callToAction}</Link>
+      <img className="logo" src={props.logo} alt={props.alt}></img>
+       <div className="greeting_container">
+        <p className="greeting"> {props.greeting} </p>
+        <Link className="call_to_action">{props.callToAction}</Link>
+        </div>
       </div>
 
   )
--- src/components/splash_screen/Welcome.js
+++ src/components/splash_screen/Welcome.js
@@ -2,13 +2,24 @@ import React from "react";
 import Content from "./Content"
 
 export default function Welcome() {
+  const assets = {
+      images: [
+         {
+            type: 'logo',
+          src: './assets/images/elvin.svg',
+            alt: 'site logo',
+            name: 'Elvin Garcia',
+          url: '/',
+          }
+        ],
+    }
+
+  const content = assets.images.map((obj, i) => <Content name={obj.name} logo={obj.src} greeting={"Hi, I'm Elvin."} callToAction={"View my porfolio"} key={i} alt={obj.alt } />)
+
 
   return (
-    <Content
-      name={"Elvin"}
-      logo={"logo goes here"}
-      greeting={"Hi, I'm Elvin."}
-      callToAction={"View my porfolio"}
-    />
+      <>
+      {content}
+      </>
  )
 }
