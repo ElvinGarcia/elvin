989a83fa6f02d404884723a0213336c0fc01e178
Elvin <Elvin@ElvinGarcia.com>
2022-11-07 21:40:16 -0500

    contact form opens mail client and prefiles email for vistor. Afterwards it clears the form.

--- src/components/main/contact/Content.js
+++ src/components/main/contact/Content.js
@@ -1,27 +1,40 @@
-import React, { Component } from "react";
 
-class Content extends Component{
+import React,{useRef} from "react";
 
-  render() {
+
+function Content() {
+
+  const form = useRef();
+
+  const formSubmit = event => {
+    event.preventDefault();
+
+    const name = form.current.user_name.value;
+    const email = form.current.user_email.value;
+    const message = form.current.user_message.value;
+    window.location.href = `mailto:dev@ElvinGarcia.com?subject=${name}:${email}&body=${message}`;
+    form.current.user_message.value = " "
+    form.current.user_email.value = " "
+    form.current.user_name.value=" "
+
+}
+
     return (
       <div className="contact_me form">
         <h2> Say Hello!</h2>
-        <form method="post" name="contact">
+        <form method="post" name="contact" ref={form} onSubmit={formSubmit}>
           <ul>
             <li>
-              <label htmlFor="name">Name
-              </label>
-              <input type="text" id="name" name="user_name" />
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" name="user_name"/>
             </li>
             <li>
-              <label htmlFor="mail">E-mail
-              </label>
+              <label htmlFor="mail">E-mail</label>
               <input type="email" id="mail" name="user_email" />
             </li>
             <li>
-              <label htmlFor="msg">Message
-              </label>
-              <textarea id="msg" name="user_message"></textarea>
+              <label htmlFor="msg">Message</label>
+              <textarea id="msg" name="user_message" ></textarea>
             </li>
             <li className="button">
               <button name="send" type="submit">Send</button>
@@ -31,7 +44,7 @@ class Content extends Component{
       </div>
     )
   }
-}
+
 
 
 export default Content;
