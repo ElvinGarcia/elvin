eea991004b3661a46c75caed01ec4b8fdf2d1c5a
github username <github email address>
2022-11-30 15:23:04 -0500

    updated resume section to included an iframe

--- src/components/resume/Content.js
+++ src/components/resume/Content.js
@@ -5,14 +5,8 @@ const Content = (props) => {
   const src="https://docs.google.com/document/d/18Em-uTKuPkMoNZWt_l2GjSm3kw0OTdl6944SjNOOljg/edit?usp=sharing&includes_info_params=true"
   return (
     <section className="resume_content">
-      <embed
-        type="application/pdf"
-        title="Resume"
-       src={src}
-       width="818"
-        height="999">
-
-       </embed>
+      <iframe src="https://docs.google.com/document/d/e/2PACX-1vRhYiDEctLPU0buQvLxKAhTlROpN9-e1EAzXNU-qYBkVIhz2bkFQN6FdAQIJyVH_p9qQwBUrPLzQw1T/pub?embedded=true" height="990" width="100%;" scrolling="no" title="Resume" frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen= {true}>
+</iframe>
     </section>
 )
 
