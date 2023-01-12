ea8e1fcd9b159e3cc0f288f996eb786eb5fddd8a
github username <github email address>
2023-01-12 13:29:03 -0500

    replace embeded with PDF

--- src/components/resume/Content.js
+++ src/components/resume/Content.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Pdf from '../../siteData/dev4f_Resume_Personal.pdf';
 
 
 const Content = (props) => {
   return (
+
     <section className="resume_content">
-      <iframe src="https://docs.google.com/document/d/e/2PACX-1vRhYiDEctLPU0buQvLxKAhTlROpN9-e1EAzXNU-qYBkVIhz2bkFQN6FdAQIJyVH_p9qQwBUrPLzQw1T/pub?embedded=true" height="990" width="100%;" scrolling="no" title="Resume" frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen= {true}>
-</iframe>
+      <a href = {Pdf} target = "_blank" rel="noreferrer">Download Pdf</a>
+      {/* <iframe src="https://docs.google.com/document/d/e/2PACX-1vRhYiDEctLPU0buQvLxKAhTlROpN9-e1EAzXNU-qYBkVIhz2bkFQN6FdAQIJyVH_p9qQwBUrPLzQw1T/pub?embedded=true" height="990" width="100%;" scrolling="no" title="Resume" frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen= {true}>
+</iframe> */}
     </section>
 )
 
