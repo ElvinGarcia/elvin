ab0bb1509a3cca7162ea04bbc17db9b00eebde49
github username <github email address>
2023-01-12 13:33:22 -0500

    removed comments

--- src/components/resume/Content.js
+++ src/components/resume/Content.js
@@ -7,8 +7,6 @@ const Content = (props) => {
 
     <section className="resume_content">
       <a href = {Pdf} target = "_blank" rel="noreferrer">Download Pdf</a>
-      {/* <iframe src="https://docs.google.com/document/d/e/2PACX-1vRhYiDEctLPU0buQvLxKAhTlROpN9-e1EAzXNU-qYBkVIhz2bkFQN6FdAQIJyVH_p9qQwBUrPLzQw1T/pub?embedded=true" height="990" width="100%;" scrolling="no" title="Resume" frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen= {true}>
-</iframe> */}
     </section>
 )
 
