ce6bce99358d96689a6386ad9e08057e7142530a
github username <github email address>
2023-02-24 10:28:18 -0500

    fix rel atrribute

--- src/components/footer/FooterMenu.js
+++ src/components/footer/FooterMenu.js
@@ -24,7 +24,7 @@ function FooterMenu(props){
     );
 
   const social_links = props.social_links.map((obj, id) => {
-    return obj.type === "social_links" ? <a href={obj.href} name={obj.name} target={"_blank"} rel={"me"} key={id}>{obj.svg}</a> : null;
+    return  obj.name !== "mastodon" ? <a href={obj.href} name={obj.name} target={"_blank"} rel={"noreferrer"} key={id}>{obj.svg}</a> : <a href={obj.href} name={obj.name} rel="me"key={id}>{obj.svg}</a>;
     }
     );
 
